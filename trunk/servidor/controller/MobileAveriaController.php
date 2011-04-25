8db15667a80c026e316106309c79d72746f57114
joserafaelguevara@gmail.com <joserafaelguevara@gmail.com@3f996f07-1ecf-016a-c652-e72129dd8127>
2011-04-25 18:46:34 +0000

    Implementacion de Reparar y Actualizar en MobileAveria

--- trunk/servidor/controller/MobileAveriaController.php
+++ trunk/servidor/controller/MobileAveriaController.php
@@ -6,7 +6,7 @@
 **@version v0.9 04/2011
 */
 
-//@include_once('../model/MobileSenal.php');
+@include_once('../model/MobileSenal.php');
 @include_once('../model/MobileAveria.php');
 
 date_default_timezone_set("America/Caracas");
@@ -41,21 +41,48 @@ switch ($id_op) {
 	case 2: // Notificar Averia
 		$id_senal		= $_POST['id_senal'];
 		$fecha_averia	= $_POST['fechaAveria'];
-		$login_registro	= $_POST['loginRegistro'];	// 1
-		//$login_reparac	= $_POST['loginReparacion'];
-		//$fecha_reparac	= $_POST['fechaReparacion'];
+		$login_registro	= $_POST['loginRegistro'];
+		//$login_repara	= $_POST['loginReparacion'];
+		//$fecha_repara	= $_POST['fechaReparacion'];
 		$id_motivo		= $_POST['idMotivo'];
-		//$id_status		= $_POST['idStatus'];
+		//$id_status	= $_POST['idStatus'];
 		$observaciones	= $_POST['observaciones'];
+		$id_estad_sen 	= $_POST['estado'];
+		$sen_ave 		= $_POST['averia'];
 		
 		$averia = new MobileAveria;
-		$averia->registrar( $id_senal, $id_motivo, $observaciones, $login_registro, $fecha_averia);
+		$averia->registrar( $id_senal, $id_motivo, $observaciones, $login_registro, $fecha_averia );
+		
+		$senal = new MobileSenal;
+		$senal->modificarEstado( $id_senal, $id_estad_sen, $sen_ave );
 		
 		echo "OK";
 		
 	break;
 	case 3: //actualizar averia
+		$id_averia		= $_POST['id_averia'];
+		$fecha_averia	= $_POST['fechaAveria'];
+		$login_registro	= $_POST['loginRegistro'];
+		$id_motivo		= $_POST['idMotivo'];
+		//$observaciones	= $_POST['observaciones'];
+		
+		$averia = new MobileAveria;
+		$averia->actualizar( $id_averia, $login, $fecha, $id_motivo );
+		
 		echo 'OK';
+	break;
+	case 4: //reparar averia
+		$id_averia			= $_POST['id_averia'];
+		$id_senal			= $_POST['id_senal'];
+		$fecha_reparacion	= $_POST['fecha'];
+		$login_reparacion	= $_POST['login'];
+		
+		$averia = new MobileAveria;
+		$averia->reparar( $id_averia, $login_reparacion, $fecha_reparacion ); 
+		
+		$senal = new MobileSenal;
+		$senal->modificarEstado( $id_senal, 1, "N" );
+		
 	break;
 	default:
     break;  
--- trunk/servidor/model/MobileAveria.php
+++ trunk/servidor/model/MobileAveria.php
@@ -31,11 +31,10 @@ class MobileAveria {
 			$xml .= '<averia id="'.$temparray['id_averia'].'">'; 
 			$xml .= '<senal>'.$temparray['id_senal'].'</senal>';
 			$xml .= '<motivo>'.$temparray['id_motivo'].'</motivo>';
-			$xml .= '<fecha>'.$temparray['fecha_registro'].'</fecha>';
+			//$xml .= '<fecha>'.$temparray['fecha_registro'].'</fecha>';
 			$xml .= '<login>'.$temparray['login_registro'].'</login>';
 			$xml .= '</averia>';
 		}
-		
 		$xml .= '</rst>';
 		
 		$conexion->desconectar();
@@ -53,8 +52,8 @@ class MobileAveria {
 		$fmt_fecha_ave	= $conexion->darFormatoFecha($fecha_averia);
 		$fecha_actual 	= $conexion->getSQLTimeStamp();
 		
-		$sql = "insert into ".$_SESSION['db_rst'].$_SESSION['schema_db'].".$this->trst_datos_ave ( id_senal, fecha_averia, login_registro, fecha_registro, id_motiv_ave, id_status_ave, observaciones ) 
-				values ( $id_senal, $fmt_fecha_ave, '$login', $fecha_actual, $id_motiv_ave, 1, '".strtoupper($observaciones)."' )";
+		$sql = "INSERT INTO ".$_SESSION['db_rst'].$_SESSION['schema_db'].".$this->trst_datos_ave ( id_senal, fecha_averia, login_registro, fecha_registro, id_motiv_ave, id_status_ave, observaciones ) 
+				VALUES ( $id_senal, $fmt_fecha_ave, '$login', $fecha_actual, $id_motiv_ave, 1, '".strtoupper($observaciones)."' )";
 		
 		$fp = fopen('sql.txt','a' ); 
 		fwrite($fp,$sql."\r\n");
@@ -62,12 +61,68 @@ class MobileAveria {
 		
 		$this->respuesta = $conexion->consultar($sql) 
 			or die("No se pudo registrar la Señal de Tránsito");
-			
+		
+		
+		
 		$conexion->desconectar();
 		
 		$auditoria 	= new Auditoria;
 		$auditoria->insertar( "707" );
 	}
-
+	
+	public function actualizar ( $id_averia, $login, $fecha, $id_motivo ) {
+	
+		$conexion 	= new EnlaceBD;
+		$var 		= $conexion->conectar( $_SESSION['db_rst'] );		
+		
+		$fmt_fecha	= $conexion->darFormatoFecha($fecha_reparacion);
+		$fecha_modificacion  = $conexion->getSQLTimeStamp();
+				
+		$sql = "UPDATE ".$_SESSION['db_rst'].$_SESSION['schema_db'].".$this->trst_datos_ave SET 
+					login_registro = '$login_reparacion',
+					fecha_averia = $fmt_fecha,
+					id_motiv_ave = $id_motivo
+				WHERE id_averia = '$id_averia'";				
+		
+		$fp = fopen('sql.txt','a' ); 
+		fwrite($fp,$sql."\r\n");
+		fclose($fp);
+		
+		$this->respuesta = $conexion->consultar($sql) 
+			or die("No se pudo registrar la Señal de Tránsito");
+		
+		$conexion->desconectar();
+		
+		$auditoria 	= new Auditoria;
+		$auditoria->insertar( "708" );
+	
+	}
+	
+	public function reparar ( $id_averia, $login_reparacion, $fecha_reparacion	) {
+	
+		$conexion 	= new EnlaceBD;
+		$var 		= $conexion->conectar( $_SESSION['db_rst'] );		
+		
+		$fmt_fecha_rep	= $conexion->darFormatoFecha($fecha_reparacion);
+		$fecha_modificacion  = $conexion->getSQLTimeStamp();
+				
+		$sql = "UPDATE ".$_SESSION['db_rst'].$_SESSION['schema_db'].".$this->trst_datos_ave SET 
+					login_reparacion = '$login_reparacion',
+					fecha_reparacion = $fmt_fecha_rep
+				WHERE id_averia = '$id_averia'";				
+		
+		$fp = fopen('sql.txt','a' ); 
+		fwrite($fp,$sql."\r\n");
+		fclose($fp);
+		
+		$this->respuesta = $conexion->consultar($sql) 
+			or die("No se pudo registrar la Señal de Tránsito");
+		
+		$conexion->desconectar();
+		
+		$auditoria 	= new Auditoria;
+		$auditoria->insertar( "708" );
+	
+	}
 } // End Class
 ?>
